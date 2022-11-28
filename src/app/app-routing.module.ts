3ef0738fafbc56aa63565f6a998cad936544d289
mssalkhalifah <mssalkhalifah@gmail.com>
2022-11-28 22:53:36 +0300

    Added eslint and prettier

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
-const routes: Routes = [
-  {path: '', component: AppComponent}
-];
+const routes: Routes = [{ path: '', component: AppComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
